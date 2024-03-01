919942bfc6d638e033c60c9388e95a097f7f190c
hiabed <abed.hassani.idrissi@gmail.com>
2024-03-01 21:24:46 +0100

    parse_header() cleaner code

--- for_header.cpp
+++ for_header.cpp
@@ -8,22 +8,14 @@ std::string parse_header(std::string to_join, std::string &content_type, std::st
     content_length = "Content-Length";
     while(getline(stream, token, '\n'))
     {
+        // Remove any trailing newline characters
+        token.erase(token.end() - 1); // \r;
         std::string str_type = token.substr(0, 12);
         std::string str_length = token.substr(0, 14);
-
         if (str_length == content_length)
-        {
-            content_length = token.substr(16, token.substr(16).size() - 1); // -1 of \r;
-            std::cout << "cl: " << content_length << std::endl;
-            std::cout << "cls: " << content_length.size() << std::endl;
-        }
-        if (str_type == content_type)
-        {
-            //9 number handled only html file; i.g for text/jpg should be 10;
-            content_type = token.substr(14, token.substr(14).size() - 1); // -1 of \r;
-            std::cout << "ct: " << content_type << std::endl;
-            std::cout << "cts: " << content_type.size() << std::endl;
-        }
+            content_length = token.substr(16); // Exclude "Content-Length: "
+        else if (str_type == content_type)
+            content_type = token.substr(14); // Exclude "Content-Type: "
     }
     return "";
 }
