c1b1f17324b9d518fe0bc379e39b8f33fee8670c
hiabed <abed.hassani.idrissi@gmail.com>
2024-03-10 17:10:43 +0000

    cleaner code

--- post.cpp
+++ post.cpp
@@ -2,8 +2,6 @@
 
 /*-- My Global variables --*/
 
-//for opening the file;
-int flag = 0;
 std::string fileName;
 std::ofstream outFile;
 std::string extension;
@@ -23,7 +21,10 @@ std::string hexa;
 std::string remain;
 int f = 0;
 
-void open_unic_file()
+// for debugging;
+int count = 0;
+
+void open_unic_file(std::string contentType)
 {
     map m = read_file_extensions("fileExtensions");
     map::iterator it = m.find(contentType);
@@ -40,7 +41,7 @@ bool post_method(std::string buffer)
     if (buffer.find("\r\n\r\n") != std::string::npos && f == 0)
     {
         parse_header(buffer, contentType, content_length, transfer_encoding);
-        open_unic_file();
+        open_unic_file(contentType);
         buffer = buffer.substr(buffer.find("\r\n\r\n") + 4);
     }
     if (transfer_encoding != "chunked")
@@ -50,23 +51,18 @@ bool post_method(std::string buffer)
     return false;
 }
 
-std::string switch_to_hexa(std::string buffer)
+std::string switch_to_hexa(std::string remain)
 {
-    hexa = buffer.substr(0, buffer.find("\r\n"));
+    hexa = remain.substr(0, remain.find("\r\n"));
     ss << std::hex << hexa;
     ss >> chunk_length;
     ss.str("");
+    if (hexa == "10000")
+        count++;
     std::cout << hexa << std::endl;
-    return buffer.substr(buffer.find("\r\n") + 2);
-}
-
-std::string keep_the_remaining(std::string buffer)
-{
-    outFile << buffer.substr(0, buffer.find("\r\n"));
-    // change buffer value;
-    buffer = buffer.substr(buffer.find("\r\n") + 2); // hexa\r;
-    // current chunk done;
-    return switch_to_hexa(buffer);
+    std::cout << count << std::endl;
+    usleep(500000);
+    return remain.substr(remain.find("\r\n") + 2); // return only the remaining part of the body without hexa\r\n;
 }
 
 bool chunked(std::string buffer)
@@ -75,26 +71,23 @@ bool chunked(std::string buffer)
     {
         if (f == 0)
         {
-            // if (buffer.find("\r\n") != std::string::npos)
             buffer = switch_to_hexa(buffer);
             f = 1;
         }
-        body_size += buffer.size();
-        if (body_size >= chunk_length)
+        remain += buffer; //add the buffer to the reamin (remaining);
+        if (remain.find("\r\n") != std::string::npos && remain.find("\r\n", remain.find("\r\n") + 2) != std::string::npos)
         {
-            buffer = keep_the_remaining(buffer);
-            body_size = buffer.size();
+            outFile << remain.substr(0, remain.find("\r\n"));
+            if (remain.find("\r\n0\r\n\r\n") != std::string::npos)
+            {
+                std::cout << "done.\n";
+                outFile.close();
+                f = 0;
+                return true;
+            }
+            remain = switch_to_hexa(remain.substr(remain.find("\r\n") + 2));
+            // keep only the body part without hexa\r\n;
         }
-        if (chunk_length == 0 || buffer.find("\r\n0\r\n\r\n") != std::string::npos)
-        {
-            outFile << buffer.substr(0, buffer.find("\r\n0\r\n\r\n"));
-            outFile.close();
-            f = 0;
-            body_size = 0;
-            return true;
-        }
-        outFile << buffer;
-        buffer.clear();
     }
     else
         std::cerr << "Error opening file for appending.\n";
@@ -110,8 +103,8 @@ bool binary(std::string buffer)
         if (body_size == atoi(content_length.c_str()))
         {
             outFile.close();
-            flag = 0;
             body_size = 0;
+            f = 0;
             return true;
         }
     }
--- webserv.hpp
+++ webserv.hpp
@@ -28,7 +28,7 @@ bool post_method(std::string buffer);
 std::string readUntilSeparator(std::string buffer);
 bool binary(std::string buffer);
 bool chunked(std::string buffer);
-void open_unic_file();
-
+void open_unic_file(std::string contentType);
+std::string switch_to_hexa(std::string remain);
 
 #endif
